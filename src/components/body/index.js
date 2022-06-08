cd6bb22c34798825e5628142629ac4168741785c
xiaowang <2805528068@qq.com>
2022-06-08 14:52:41 +0800

    路由的基本使用

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './index.less'
+import { Route } from 'react-router-dom'
+import Home from './Home'
 export default class index extends Component {
   render() {
-    return (
+    return ( 
       <div className='body'>
         <div className="container">
           <div className="row">
-            
+            <Route path='/Home' component={Home} />
           </div>
         </div>
       </div>
